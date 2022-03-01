bb126914d0c1d3aa36b14197aa37099cd83c0638
BlueMist <37814898+BlueM2ST@users.noreply.github.com>
2022-03-01 10:29:22 -0400

    Added initialization

--- system/scripts/Main.gd
+++ system/scripts/Main.gd
@@ -1,5 +1,58 @@
 extends Node2D
 
 
+signal next_line
+signal gameInitialized
+
+onready var fullGameScript : Dictionary = {}
+onready var languages : Dictionary = {}
+onready var scriptDict : Dictionary = {}
+onready var funcRefDict : Dictionary = {}
+onready var nodePaths : Dictionary = {}
+
+onready var camera : Camera2D = Camera2D.new()
+onready var tween : Tween = Tween.new()
+onready var expression : Expression = Expression.new()
+
+
 func _ready():
-	pass
+	initializeGameSystem()
+	yield(self, "gameInitialized")
+	print("game initialized")
+
+
+func initializeGameSystem():
+	# nodes that are part of the main scene at runtime will not be used
+	for child in get_children():
+		child.queue_free()
+	
+	# load all game script files
+	var scriptLoader : Node = Node.new()
+	scriptLoader.set_script(load("res://system/scripts/ScriptLoader.gd"))
+	add_child(scriptLoader)
+	yield(scriptLoader, "all_scripts_loaded")
+	scriptLoader.queue_free()
+	print("scripts loaded")
+	
+	
+	
+	emit_signal("gameInitialized")
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
